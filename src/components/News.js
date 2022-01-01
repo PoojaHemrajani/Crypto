e0132884c71cc43c58235129750ee085164b111d
shivani agarwal <shiviagarwal6161@gmail.com>
2022-01-01 15:28:48 +0530

    updated news component

--- src/components/News.js
+++ src/components/News.js
@@ -1,69 +1,73 @@
 import React, { useEffect, useState } from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
 
 export default function News() {
 
     const numbers = [1, 2, 3, 4, 5];
-    
+    const [isLoading, setIsLoading] = useState(true);
+    const LOADING_TIME = 0; // The loading screen will show for 2 Seconds.
+
     const [data, setData] = useState([]);
 
-    // useEffect(() => {
-    //     getData();
-    // })
-
-    const getData = () => {
-        fetch("https://crypto-news-live.p.rapidapi.com/news/coindesk", {
-            method: 'GET', // or 'PUT'
-            headers : {
-                'x-rapidapi-key': '1a2c45901cmsh114ef477c1ea3f0p10f461jsn3ce1659d6f51',
-                'x-rapidapi-host': 'crypto-news-live.p.rapidapi.com'
-            }
-        })
-            .then(res => res.json())
-            .then(
-              (result) => {
-                setData(result);
-                // console.log(result);
-              },
-              // Note: it's important to handle errors here
-              // instead of a catch() block so that we don't swallow
-              // exceptions from actual bugs in components.
-              (error) => {
-                this.setState({
-                  isLoaded: true,
-                  error
-                });
-              }
-        )
+    useEffect(() => {
+        const getData = async () => {
+            await fetch("https://crypto-news-live.p.rapidapi.com/news/coindesk", {
+                method: 'GET', // or 'PUT'
+                headers: {
+                    'x-rapidapi-key': 'fa908767b9msh34e56aeb8c78b68p14342ejsnf30701e89743',
+                    'x-rapidapi-host': 'crypto-news-live.p.rapidapi.com'
+                }
+            })
+                .then(res => res.json())
+                .then(
+                    (result) => {
+                        console.log('result: ', result);
+                        setData(result);
+                    },
+                    // Note: it's important to handle errors here
+                    // instead of a catch() block so that we don't swallow
+                    // exceptions from actual bugs in components.
+                    (error) => {
+                        console.log('error', error);
+                    }
+                )
+            setTimeout(() => setIsLoading(false), LOADING_TIME);
+        }
+        getData();
+        // setIsLoading(false);
+    }, [])
+    console.log('data: ', data);
+    if (isLoading) {
+        // we haven't finished checking for the token yet
+        return <>Loading...</>;
     }
 
-    getData();
-    // console.log(data);  
+
+    // getData();
+
 
     return (
-        <div style={{display: 'flex', flexWrap: 'wrap'}}>
-            {numbers.map((number) => (
-                <Card sx={{ maxWidth: 320 }} style={{margin: 15}}>
-                  <CardActionArea>
+        <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+            {data.map((data, i) => (
+                <Card key={i} sx={{ maxWidth: 320 }} style={{ margin: 15 }}>
                     <CardContent>
-                      <Typography gutterBottom variant="h5" component="div">
-                        Lizard
-                      </Typography>
-                      <Typography variant="body2" color="text.secondary">
-                        Lizards are a widespread group of squamate reptiles, with over 6,000
-                        species, ranging across all continents except Antarctica
-                      </Typography>
+                        <Typography gutterBottom variant="h5" component="div">
+                            {data.title}
+                        </Typography>
+                        <CardActionArea>
+                            <Typography variant="body2" color="text.secondary">
+                                <a href={data.url}>{data.url}</a>
+                            </Typography>
+                        </CardActionArea>
                     </CardContent>
-                  </CardActionArea>
                 </Card>
             )
             )
             }
-        </div>
+        </div >
     )
 }
 
